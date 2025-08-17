b596e7330ebcf913cf89ba709d4d11a48ebc3f70
quoituparle <daiyue.han@outlook.com>
2025-08-17 17:11:45 +0800

    models.py

--- main/test.py
+++ main/test.py
@@ -2,10 +2,7 @@ import os
 from google import genai
 from dotenv import load_dotenv
 
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client()
+load_dotenv()
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
+key = os.getenv("api_key")
+print(key)
